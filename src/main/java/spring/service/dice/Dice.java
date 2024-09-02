34a1224e4fb19a3a07470a10e1f6707999e0f05d
bitcamp <bitcamp@BIT>
2024-09-02 16:21:31 +0900

    09.02.월 Bean Container, IoC, IoC Container

---
+++ src/main/java/spring/service/dice/Dice.java
@@ -0,0 +1,9 @@
+package spring.service.dice;
+
+public interface Dice {
+	
+	public int getValue();
+	
+	public void selectedNumber();
+	
+}
---
+++ src/main/java/spring/service/dice/impl/DiceCImpl.java
@@ -0,0 +1,29 @@
+package spring.service.dice.impl;
+
+import java.util.Random;
+
+import spring.service.dice.Dice;
+
+public class DiceCImpl implements Dice {
+
+	int value;
+	
+	public DiceCImpl() {
+		System.out.println("::"+getClass().getName()+" »ý¼ºÀÚ....");
+	}
+
+	@Override
+	public int getValue() {
+		return value;
+	}
+	
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	@Override
+	public void selectedNumber() {
+		value = new Random().nextInt(6) + 1;
+	}
+
+}
